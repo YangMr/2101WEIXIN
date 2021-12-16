5af243fc849c46f38523537d26752988d20fb3f2
cstp <759168442@qq.com>
2021-12-16 15:38:55 +0800

    feta:wxml语法

---
+++ 2day/readme.md
@@ -0,0 +1,20 @@
+# 课堂笔记
+
+## 一、小程序的页面配置
+
+- navigationBarBackgroundColor 导航栏背景颜色 颜色值只能为16进制
+- navigationBarTextStyle  导航栏标题颜色，仅支持 black / white
+- navigationBarTitleText  导航栏标题文字内容
+- navigationStyle   导航栏样式，仅支持以下值： default 默认样式 custom 自定义导航栏，只保留右上角胶囊按钮。
+- backgroundColor   窗口的背景色
+- backgroundTextStyle   下拉 loading 的样式，仅支持 dark / light
+- backgroundColorTop   
+- backgroundColorBottom   
+- enablePullDownRefresh      是否开启当前页面下拉刷新
+- onReachBottomDistance      页面上拉触底事件触发时距页面底部距离，单位为px。
+- disableScroll              设置为 true 则页面整体不能上下滚动
+- usingComponents            页面自定义组件配置
+
+## 二、wxml语法
+
+## 三、案例
---
+++ 2day/two-program/pages/index/index.js
@@ -0,0 +1,155 @@
+// pages/index/index.js
+Page({
+
+  selectTag(event){
+
+    let i = event.currentTarget.dataset.i
+
+    console.log(i)
+    this.setData({
+      activeIndex : i
+    })
+  },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg : "hello world",
+    array : [],
+    articleList : [
+      {
+        id : 1,
+        title : "习大大访问美国1",
+        status : 0
+      },
+      {
+        id : 2,
+        title : "习大大访问美国2",
+        status : 1
+      },
+      {
+        id : 3,
+        title : "习大大访问美国3",
+        status : 2
+      },
+      {
+        id : 4,
+        title : "习大大访问美国4",
+        status : 3
+      },
+      {
+        id : 5,
+        title : "习大大访问美国5",
+        status : 2
+      },
+      {
+        id : 1,
+        title : "习大大访问美国1",
+        status : 0
+      },
+      {
+        id : 2,
+        title : "习大大访问美国2",
+        status : 1
+      },
+      {
+        id : 3,
+        title : "习大大访问美国3",
+        status : 2
+      },
+      {
+        id : 4,
+        title : "习大大访问美国4",
+        status : 3
+      },
+      {
+        id : 5,
+        title : "习大大访问美国5",
+        status : 2
+      }
+    ],
+    labelList : [],
+    activeIndex : 0,
+    status : 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //发送完请求并且获取到数据了
+    const res = [{id:1,name : "jack"},{id:2,name : "rose"}];
+    this.setData({
+      array : res
+    })
+
+    //当页面加载的时候请求数据   wx.request()
+    wx.request({
+      url : "http://m.mengxuegu.com/api/article/api/category/label/list",
+      method : "GET",
+      data : {},
+      success :(response)=>{
+        console.log(response.data.data)
+        this.setData({
+          labelList : response.data.data
+        })
+      },
+      fail : function (err){
+        console.log(err)
+      },
+      complete : function (){
+
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

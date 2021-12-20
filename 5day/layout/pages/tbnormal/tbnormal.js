8883054760a2293049eff0c4344dfc59fc2b0aa0
cstp <759168442@qq.com>
2021-12-20 09:40:34 +0800

    feta:实现吕形布局以及上下栏布局

---
+++ 5day/layout/pages/tbnormal/tbnormal.js
@@ -0,0 +1,72 @@
+// pages/tbnormal/tbnormal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mainHeight : ""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    const res = wx.getSystemInfoSync().windowHeight
+
+    let mainHeight = res - 120
+
+    this.setData({
+      mainHeight : mainHeight
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
